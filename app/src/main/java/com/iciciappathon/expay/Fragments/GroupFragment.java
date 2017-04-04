b09dce0b53a56748c7ea448b1d8cfa99624a66a4
Hiren Patel <hiren8890@gmail.com>
2017-04-04 22:16:47 +0530

    added floating button

--- app/src/main/java/com/iciciappathon/expay/Fragments/GroupFragment.java
+++ app/src/main/java/com/iciciappathon/expay/Fragments/GroupFragment.java
@@ -1,20 +1,27 @@
 package com.iciciappathon.expay.Fragments;
 
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.iciciappathon.expay.Activities.CreateGroupActivity;
 import com.iciciappathon.expay.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GroupFragment extends Fragment {
+public class GroupFragment extends Fragment{
 
-
+    private FloatingActionButton mBtnCreateGroup;
+    private ListView mGroupListView;
+    private View mParentView;
     public GroupFragment() {
         // Required empty public constructor
     }
@@ -23,8 +30,24 @@ public class GroupFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_group, container, false);
+        mParentView = inflater.inflate(R.layout.fragment_group, container, false);
+
+        initializeUiElements(mParentView);
+
+        mBtnCreateGroup.setBackgroundColor(getActivity().getColor(R.color.colorPrimary));
+        mBtnCreateGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent createGroupIntent = new Intent(getActivity(), CreateGroupActivity.class);
+                startActivity(createGroupIntent);
+            }
+        });
+
+        return mParentView;
     }
 
+    private void initializeUiElements(View view) {
+        mBtnCreateGroup = (FloatingActionButton) view.findViewById(R.id.btn_create_group);
+        mGroupListView = (ListView) view.findViewById(R.id.lv_grouplistView);
+    }
 }
