c73d128713d9c430df54d667546e03a0efe7dd03
Hiren Patel <hiren8890@gmail.com>
2017-04-04 20:29:14 +0530

    Added toolbar and tabs

--- app/src/main/java/com/iciciappathon/expay/Activities/HomeActivity.java
+++ app/src/main/java/com/iciciappathon/expay/Activities/HomeActivity.java
@@ -1,56 +1,75 @@
 package com.iciciappathon.expay.Activities;
 
-import android.app.FragmentManager;
+import android.annotation.TargetApi;
 import android.content.res.Configuration;
+import android.os.Build;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
 
-import com.iciciappathon.expay.Fragments.HomeFragment;
+import com.iciciappathon.expay.Adapters.ViewPagerAdapter;
+import com.iciciappathon.expay.Fragments.GroupFragment;
 import com.iciciappathon.expay.Fragments.NavigationFragment;
+import com.iciciappathon.expay.Fragments.TransactionFragment;
 import com.iciciappathon.expay.R;
 
 public class HomeActivity extends AppCompatActivity {
-    HomeFragment mHomeFragment;
     NavigationFragment mNavFragment;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
-    ActionBar mActionBar;
+    private Toolbar appToolBar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        drawerLayout = (DrawerLayout) findViewById(R.id.poi_homedrawerLayout);
+        setupToolbar();
 
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout = (DrawerLayout) findViewById(R.id.homedrawerLayout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, appToolBar , R.string.open, R.string.close);
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
-
-        mActionBar =  getActionBar();
-        if(mActionBar != null) {
-            mActionBar.setDisplayHomeAsUpEnabled(true);
-            mActionBar.setTitle(getString(R.string.app_name));
-        }
-        mHomeFragment = new HomeFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.gridfragment_container,mHomeFragment).commit();
-
 
         mNavFragment = new NavigationFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.homenavfragment_container, mNavFragment).commit();
 
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
 
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new GroupFragment(), "Groups");
+        adapter.addFragment(new TransactionFragment(), "Transaction");
+        viewPager.setAdapter(adapter);
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         actionBarDrawerToggle.syncState();
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void setupToolbar() {
+        appToolBar = (Toolbar)findViewById(R.id.toolbar);
+        if(appToolBar != null) {
+            setSupportActionBar(appToolBar);
+            appToolBar.setTitle(getString(R.string.app_name));
+            appToolBar.setTitleTextColor(getColor(R.color.white));
+        }
     }
 
     @Override
