42460b81fefbe237bb4c34d024de356f24cb0e09
Hiren Patel <hiren8890@gmail.com>
2017-04-10 02:06:04 +0530

    Transaction Fragment Changes

--- app/src/main/java/com/iciciappathon/expay/Fragments/TransactionFragment.java
+++ app/src/main/java/com/iciciappathon/expay/Fragments/TransactionFragment.java
@@ -1,30 +1,53 @@
 package com.iciciappathon.expay.Fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.iciciappathon.expay.Adapters.TransactionListAdapter;
+import com.iciciappathon.expay.Database.DatabaseHandler;
+import com.iciciappathon.expay.POJOBeans.Transaction;
 import com.iciciappathon.expay.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.ArrayList;
+
 public class TransactionFragment extends Fragment {
 
-
-    public TransactionFragment() {
-        // Required empty public constructor
-    }
-
+    private ListView lvTransactionList;
+    private View mParentView;
+    private TransactionListAdapter transactionListAdapter;
+    private ArrayList<Transaction> mTransactionArrayList = new ArrayList<>();
+    private Context mContext = null;
+    DatabaseHandler databaseHandler;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaction, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        mParentView = inflater.inflate(R.layout.fragment_transaction, container, false);
+
+        initializeUi();
+
+        setdata();
+
+        transactionListAdapter = new TransactionListAdapter(mContext,mTransactionArrayList);
+        lvTransactionList.setAdapter(transactionListAdapter);
+
+        return mParentView;
+    }
+
+    private void setdata() {
+        mTransactionArrayList.clear();
+        mTransactionArrayList = (ArrayList<Transaction>) databaseHandler.getTransactions();
+    }
+
+    private void initializeUi() {
+        mContext = getActivity().getApplicationContext();
+        databaseHandler = new DatabaseHandler(mContext);
+        lvTransactionList = (ListView) mParentView.findViewById(R.id.lvTransactionList);
     }
 
 }
