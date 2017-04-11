1df74718e9415d0f9cbd5bee5f20199cd3b3706a
Kavita Pardeshi <pardeshi.kavita25@gmail.com>
2017-04-12 01:42:21 +0530

    Adding UDP payment draft2

---
+++ app/src/main/java/com/iciciappathon/expay/UpiController/RequestParam.java
@@ -0,0 +1,12 @@
+package com.iciciappathon.expay.UpiController;
+
+/**
+ * Created by Pankaj on 4/12/2017.
+ */
+
+public class RequestParam {
+    private String mClientId;
+    private String mToken;
+    private String mAccountNo;
+    private String mNewVPA;
+}
---
+++ app/src/main/java/com/iciciappathon/expay/UpiController/UPIConstants.java
@@ -0,0 +1,13 @@
+package com.iciciappathon.expay.UpiController;
+
+/**
+ * Created by Pankaj on 4/12/2017.
+ */
+
+public class UPIConstants {
+    public static String CREATE_VPA ="https://upiservice.mvbluemix.net/banking/icicibank/createVPA?";
+    public static String FETCH_VPA ="https://upiservice.mvbluemix.net/banking/createVPA?";
+    public static String FUND_TRANSFER_VPA_TO_VPA ="https://upiservice.mvbluemix.net/banking/icicibank/upiFundTransferVToV?";
+    public static String FUND_TRANSFER_VPA_TO_ACC_NO ="https://upiservice.mvbluemix.net/banking/icicibank/upiFundTransferVToA?";
+
+}
